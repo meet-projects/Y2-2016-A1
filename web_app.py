82998dccc080341d2373275d330516922a05e57f
Student <amos17@meet.mit.edu>
2016-08-04 13:38:27 +0300

    SDVWDSV

--- web_app.py
+++ web_app.py
@@ -24,6 +24,7 @@ def main():
 
 @app.route("/flag/<string:flag_name>/")
 def flag(flag_name):
+	
 	quizzes= session.query(Quiz).filter_by(country=flag_name.capitalize()).all()
 	picture= url_for('static', filename=flag_name+".png")
 	return render_template('country.html',quizzes=quizzes, picture=picture , flag_name = flag_name.capitalize())
